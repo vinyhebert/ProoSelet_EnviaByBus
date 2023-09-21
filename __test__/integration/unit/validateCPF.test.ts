e128e9d7ce12b1deb7de74c097fdf30174247748
Vinicius Guimaraes <corevinicius@gmail.com>
2023-09-21 18:15:06 -0300

    test(validatecpf.test.ts): crie um teste para validar a funcao ValidateCPF
    Crie um teste que: 1 - valide o CPF. 2 - Verifique a quantidade de digitos. 3 - Verifique se todos
    os numeros sao iguais. 4 - faca o calculo do digito verificador 5 - faca a verificacao do formato.

---
+++ __test__/integration/unit/validateCPF.test.ts
@@ -0,0 +1,35 @@
+import { isValidCPF } from '../../../src/models/validateCPF';
+
+describe('isValidCPF', () => {
+  it('deve retornar verdadeiro para um CPF válido', () => {
+    const cpf = '123.456.789-09'; // Um CPF válido
+
+    const result = isValidCPF(cpf);
+
+    expect(result).toBe(true);
+  });
+
+  it('deve retornar falso para um CPF com todos os dígitos iguais', () => {
+    const cpf = '111.111.111-11'; // CPF com todos os dígitos iguais
+
+    const result = isValidCPF(cpf);
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar falso para um CPF com formato inválido', () => {
+    const cpf = '123456789'; // CPF com formato inválido
+
+    const result = isValidCPF(cpf);
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar falso para um CPF inválido', () => {
+    const cpf = '425.042.778-04'; // Um CPF inválido 987.654.321-00
+
+    const result = isValidCPF(cpf);
+
+    expect(result).toBe(false);
+  });
+});
