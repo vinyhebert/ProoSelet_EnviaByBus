af142e6a093f996e5d8b7464c92abc0b5c58d8ec
Vinicius Guimaraes <corevinicius@gmail.com>
2023-09-21 18:20:56 -0300

    test(validatedate.test.ts): crie um teste para validar funcao validateDate
    Crie um teste que: 1 - Valide o formato da data esperado. 2 - Valide o ano. 3 - Valide o mes. 4 -
    valide o dia. 5 - valide anos bissextos. 6 - Valide anos não bissextos. OBS: Ambos no mes de Fev.

---
+++ __test__/integration/unit/validateDate.test.ts
@@ -0,0 +1,59 @@
+import { isValidDate } from '../../../src/models/validateDate';
+
+describe('isValidDate', () => {
+  it('deve retornar verdadeiro para uma data válida no formato "YYYY-MM-DD"', () => {
+    const dateString = '2023-09-19';
+
+    const result = isValidDate(dateString);
+
+    expect(result).toBe(true);
+  });
+
+  it('deve retornar falso para uma data com formato inválido', () => {
+    const dateString = '19/09/2023';
+
+    const result = isValidDate(dateString);
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar falso para uma data com ano inválido', () => {
+    const dateString = '90905-09-19'; //9999-09-19
+
+    const result = isValidDate(dateString);
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar falso para uma data com mês inválido', () => {
+    const dateString = '2023-13-19';
+
+    const result = isValidDate(dateString);
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar falso para uma data com dia inválido', () => {
+    const dateString = '2023-09-32';
+
+    const result = isValidDate(dateString);
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar falso para uma data com fevereiro e ano não bissexto', () => {
+    const dateString = '2023-02-29';
+
+    const result = isValidDate(dateString);
+
+    expect(result).toBe(false);
+  });
+
+  it('deve retornar verdadeiro para uma data com fevereiro e ano bissexto', () => {
+    const dateString = '2024-02-29';
+
+    const result = isValidDate(dateString);
+
+    expect(result).toBe(true);
+  });
+});
